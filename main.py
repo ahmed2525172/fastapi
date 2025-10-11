bfb74d71ea6d0348617842c320ea7dc34162cbcf
Ahmed <ahmedmmohamed082@gmail.com>
2025-10-11 15:34:27 +0200

    done

--- main.py
+++ main.py
@@ -9,3 +9,13 @@ def index():
 @app.get('/about')
 def about():
     return {'data':'about page'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    #Fetch blog with id = id
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data': {'1', '2'}}
