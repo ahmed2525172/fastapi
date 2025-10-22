ed811eccad26319fde1b73f4a306cc23ad043f0b
Ahmed <ahmedmmohamed082@gmail.com>
2025-10-22 16:38:01 +0200

    done

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':'blogs'}
+@app.get('/blog')
+def index(limit=10, published: bool= True, sort: Optional[str] = None):
+    #only return the limit bolgs
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
+
+
 
 @app.get('/about')
 def about():
     return {'data':'about page'}
 
+
+
 @app.get('/blog/unpublished')
 def unpublished():
     return {'data': 'All unpublished Blogs'}
+
+
 
 
 @app.get('/blog/{id}')
@@ -20,6 +31,8 @@ def show(id: int):
     #Fetch blog with id = id
     return {'data': id}
 
+
+
 @app.get('/blog/{id}/comments')
-def comments(id):
+def comments(id, limit=10):
     return {'data': {'1', '2'}}
