bf11b8e9e4d1de42f740f7189cb76e7e9a5cf960
Ahmed <ahmedmmohamed082@gmail.com>
2025-11-09 00:56:03 +0200

    done

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,8 @@
 from sqlalchemy import Column, Integer, String
 from database import Base  # your database.py file
 
-class Item(Base):
-    __tablename__ = "users"  # must match your existing table name
-
-    Id = Column(Integer, primary_key=True, index=True)
-    Firstname = Column(String(100))           # adjust length to match your table
-    Lastname = Column(String(100))    # adjust length to match your table
+class Blog(Base):
+    __tablename__ = "items"  # must match your existing table name
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100))           # adjust length to match your table
+    body = Column(String(255))    # adjust length to match your table
--- database.py
+++ database.py
@@ -10,7 +10,7 @@ username = "root"
 password = "ahmed2628"
 hostname = "localhost"
 port = 3305
-database_name = "testdb"
+database_name = "blogs"
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{username}:{password}@{hostname}:{port}/{database_name}"
 
@@ -21,11 +21,6 @@ Base = declarative_base()
 # -------------------------
 # Define your table model
 # -------------------------
-class User(Base):
-    __tablename__ = "users"
-    Id = Column(Integer, primary_key=True, index=True)
-    Firstname = Column(String(100))
-    Lastname = Column(String(100))
 
 # -------------------------
 # Create tables in the DB
@@ -35,23 +30,16 @@ Base.metadata.create_all(bind=engine)
 # -------------------------
 # Read Excel and insert data
 # -------------------------
-df = pd.read_excel(r'c:\Users\Ahmed\Downloads\sample_people.xlsx')
+
 
 db = SessionLocal()
-
-for _, row in df.iterrows():
-    user = User(Id=row['Id'], Firstname=row['Firstname'], Lastname=row['Lastname'])
-    db.add(user)
 
 db.commit()
 
 # -------------------------
 # Query all users
 # -------------------------
-users = db.query(User).all()
 
-for user in users:
-    print(user.Id, user.Firstname, user.Lastname)
 
 # Close session
 db.close()
