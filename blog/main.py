e9c8d2ad9661835b2f2a531d816eb92b3a72e39a
Ahmed <ahmedmmohamed082@gmail.com>
2025-11-14 03:36:39 +0200

    done

--- blog/main.py
+++ blog/main.py
@@ -34,10 +34,24 @@ def destory(id, db: Session = Depends(get_db)):
     return 'done'
 
 
+
+
 @app.get('/blog')
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: Schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with id {id} not found')
+    blog.update(request.dict(), synchronize_session=False)
+    db.commit()
+    return 'updated'
+
 
 @app.get('/blog/{id}', status_code=200)
 def show(id, response: Response, db: Session = Depends(get_db)):
