f8a2e9ceb6efe5267565745ca31066394d585315
Ahmed <ahmedmmohamed082@gmail.com>
2025-12-29 04:36:30 +0200

    done

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,6 @@ from database import Base  # your database.py file
 
 class Blog(Base):
     __tablename__ = "items"  # must match your existing table name
-    id = Column(Integer, primary_key=True, index=True)
+    blog_number = Column(Integer, primary_key=True, index=True)
     title = Column(String(100))           # adjust length to match your table
     body = Column(String(255))    # adjust length to match your table
--- main.py
+++ main.py
@@ -1,43 +1,63 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from typing import Optional
+from database import engine
+from database import SessionLocal
 from pydantic import BaseModel
+from blog import Schemas, models
+from sqlalchemy.orm import Session
 
+
+models.Base.metadata.create_all(engine)
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get('/blog')
-def index(limit=10, published: bool= True, sort: Optional[str] = None):
-    #only return the limit bolgs
-    if published:
-        return {'data': f'{limit} published blogs from the db'}
-    else:
-        return {'data': f'{limit} blogs from the db'}
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
 
 
-@app.get('/about')
-def about():
-    return {'data':'about page'}
+@app.get('/blog/{id}', status_code=200)
+def show(id, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                             detail=f'The Blog with id {id} is unavailable')
+    return blog
+
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
+def create(request: Schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
 
 
-@app.get('/blog/unpublished')
-def unpublished():
-    return {'data': 'All unpublished Blogs'}
 
 
-@app.get('/blog/{id}')
-def show(id: int):
-    #Fetch blog with id = id
-    return {'data': id}
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: Schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with id {id} not found')
+    blog.update(request.dict(), synchronize_session=False)
+    db.commit()
+    return 'updated'
 
-@app.get('/blog/{id}/comments')
-def comments(id, limit=10):
-    return {'data': {'1', '2'}}
 
-class Blog(BaseModel):
-    title: str
-    body: str
-    published: Optional[bool]
 
-@app.post('/blog')
-def create_blog(request: Blog):
-    return {'data': f"Blog is Created with Title as {request.title}"}
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destory(id, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    return 'done'
