b7db56795eb432999fba4789d280fd38a9fbf134
Ahmed <ahmedmmohamed082@gmail.com>
2025-12-29 02:50:44 +0200

    done

--- blog/main.py
+++ blog/main.py
@@ -17,6 +17,19 @@ def get_db():
 
 app = FastAPI()
 
+@app.get('/blog')
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{id}', status_code=200)
+def show(id, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                             detail=f'The Blog with id {id} is unavailable')
+    return blog
 
 @app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(request: Schemas.Blog, db: Session = Depends(get_db)):
@@ -27,18 +40,6 @@ def create(request: Schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-
-@app.get('/blog')
-def all(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
-
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destory(id, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == 
-                                 id).delete(synchronize_session=False)
-    db.commit()
-    return 'done'
 
 
 @app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
@@ -52,10 +53,9 @@ def update(id, request: Schemas.Blog, db: Session = Depends(get_db)):
     return 'updated'
 
 
-@app.get('/blog/{id}', status_code=200)
-def show(id, response: Response, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                             detail=f'The Blog with id {id} is unavailable')
-    return blog
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destory(id, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    return 'done'
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
 
+
 app = FastAPI()
 
 @app.get('/blog')
@@ -11,6 +12,7 @@ def index(limit=10, published: bool= True, sort: Optional[str] = None):
         return {'data': f'{limit} published blogs from the db'}
     else:
         return {'data': f'{limit} blogs from the db'}
+
 
 @app.get('/about')
 def about():
